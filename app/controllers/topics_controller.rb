f7132fc001878cb608d200f4158e85cb6be35986
Joshua <joycejosh7@gmail.com>
2021-02-08 15:08:40 -0500

    added show and index actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,12 @@
 class TopicsController < ApplicationController
 
     before_action: redirect_if_not_logged_in
+
+    def show
+        @topic = Topic.find_by_id(params[:id])
+    end
     
+    def index
+        @topics = Topic.by_most_lists
+    end
 end
