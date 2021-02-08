254c9cc5ffb1b79841fe671000e554be7cc23339
Joshua <joycejosh7@gmail.com>
2021-02-08 14:24:39 -0500

    added new show and links

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,5 +6,44 @@ class ListsController < ApplicationController
           @error = "that topic does not exist" if params[:topic_id]
           @lists = List.all.reverse
         end
-      end
+    end
+
+    def new
+        @list = List.new
+        if params[:topic_id] 
+          @list = List.new(topic_id: params[:topic_id])
+        else
+          @list.topic = Topic.new
+        end
+    end
+    
+    def create
+        if params[:topic_id]
+          @list = current_user.lists.build(list_params)
+          @list.topic = Topic.find_by_id(params[:topic_id])
+        else
+          @list = current_user.lists.build(list_params)
+        end
+        if @list.save
+          redirect_to list_path(@list)
+        elsif @list.topic && !@list.save
+          render :action => "new", :topic_id => @list.topic.id
+        else
+          render :new
+        end
+    end
+
+    def show
+        if !@list = List.find_by_id(params[:id])
+          redirect_to lists_path
+          flash[:message] = "That list does not exist."
+        end
+    end
+
+
+    private
+
+    def list_params
+      params.require(:list).permit(:title, :description, :item_1, :item_2, :item_3, :item_4, :item_5, :item_6, :item_7, :item_8, :item_9, :item_10, :topic_id, topic_attributes: [:name])
+    end
 end
