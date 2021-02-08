864ca16231c8073c9acde35e238d6e6cf1d9f02d
Joshua <joycejosh7@gmail.com>
2021-02-08 14:06:32 -0500

    added list index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,10 @@
 class ListsController < ApplicationController
+    def index
+        if params[:topic_id] && @topic = Topic.find_by_id(params[:topic_id])
+          @lists = List.where("topic_id = '#{@topic.id}'")
+        else
+          @error = "that topic does not exist" if params[:topic_id]
+          @lists = List.all.reverse
+        end
+      end
 end
