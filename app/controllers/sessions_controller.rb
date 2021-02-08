8b8a88fbcb782f9bd7945dba9addc14661e1c674
Joshua <joycejosh7@gmail.com>
2021-02-08 13:22:32 -0500

    added new and create actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,18 @@ class SessionsController < ApplicationController
 
     def welcome
     end
-    
+
+    def new
+    end
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+          session[:user_id] = user.id
+          redirect_to lists_path
+        else
+          flash[:message] = "Incorrect username or password, please try again."
+          render :new
+        end
+    end
 end
