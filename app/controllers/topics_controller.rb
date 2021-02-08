2191bd568be3f5913df503a2473e834902daa78a
Joshua <joycejosh7@gmail.com>
2021-02-08 15:31:21 -0500

    added validations and a scope method

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
 
-    before_action: redirect_if_not_logged_in
+    before_action :redirect_if_not_logged_in
 
     def show
         @topic = Topic.find_by_id(params[:id])
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ApplicationRecord
     has_many :lists
     has_many :users, through: :lists
+
+    validates :name, uniqueness: true
+    validates :name, presence: true
+    validates :name, length: { maximum: 30 }
+    
+    scope :by_most_lists, -> {left_joins(:lists).group('topics.id').order('count(lists.topic_id) desc')}
 end
