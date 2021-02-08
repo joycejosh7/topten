377482057205dafd56537c814b327064f0d7eb26
Joshua <joycejosh7@gmail.com>
2021-02-08 15:18:35 -0500

    added accepts nested attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
     belongs_to :user
     belongs_to :topic
+    accepts_nested_attributes_for :topic, reject_if: proc { |attributes| attributes['name'].blank? }
 end
