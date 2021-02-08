f5dfdae0cbaf5c9fb46fa786c96be64052455285
Joshua <joycejosh7@gmail.com>
2021-02-08 15:17:09 -0500

    updated user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,25 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-
+    
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id
-            redirect_to lists_path
+          session[:user_id] = @user.id
+          redirect_to lists_path
         else  
-            render :new
+          render :new
         end
     end
-
-    def show
+    
+    def show #takes to current_user's account page listing all their lists
         redirect_if_not_logged_in
-        @user = User.find_by_id(params[:id])
+        @user = User.find_by_id(session[:user_id])
     end
-
+    
     private
-
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+    
+      def user_params
+          params.require(:user).permit(:username, :password)
+      end
 end
